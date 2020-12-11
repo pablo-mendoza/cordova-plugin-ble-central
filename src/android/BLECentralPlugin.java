4ec59201ffe21db2a4a97b78eac960e23039ee60
Pablo Mendoza <jpablo@gmail.com>
2020-12-11 15:10:38 -0600

    qsdasd

--- src/android/BLECentralPlugin.java
+++ src/android/BLECentralPlugin.java
@@ -760,7 +760,7 @@ public class BLECentralPlugin extends CordovaPlugin {
 
         discoverCallback = callbackContext;
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-        ScanSettings settings = new ScanSettings.Builder().setScanMode(ScanSettingsSCAN_MODE_LOW_LATENCY).build();
+        ScanSettings settings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
         if (serviceUUIDs != null && serviceUUIDs.length > 0) {
             List<ScanFilter> filters = new ArrayList<ScanFilter>();
             for (UUID uuid : serviceUUIDs) {
